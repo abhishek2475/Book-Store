6adac4efa79abcab7ed065b6d9c741a79acd0aa7
abhishek2475 <127980831+abhishek2475@users.noreply.github.com>
2024-11-23 18:14:20 +0530

    Initial Commit

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://myAtlasDBUser:ZDADDYluv69@myatlasclusteredu.xx0av.mongodb.net/?retryWrites=true&w=majority&appName=myAtlasClusterEDU';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
